5fa59e0fe1141516a4b5776783c64bf006479fc0
cxw2019 <49756543+cxw2019@users.noreply.github.com>
2021-06-19 15:31:26 +0800

    ding+reciteBySentence
    1. listen时会加上提示音ding
    2. 完成逐句跟背功能

--- Funtion/reciteBySentence.py
+++ Funtion/reciteBySentence.py
@@ -1,4 +1,5 @@
 import random
+import re
 from time import sleep
 
 from BaiduApiAccess.TTS_STT import say, listen
@@ -8,20 +9,34 @@ from DataAccess.PassageAccess import getCHNPassageTitles, getCHNPassageById
 #  整体应当为一个循环，直到用户不想继续本功能再返回
 def recite_by_sentence():
     say("好的，开始逐句跟背")
-    sleep(1)
 
     # 询问要背哪首诗
     titles = getCHNPassageTitles()
-    passageId=random.choice(titles.values())
+    passage_id=random.choice(list(titles.values()))
     say("请问你要背哪首诗")
-    text=listen()
+    text=listen().replace('。','')
+    # text=input("输入诗的题目：")
     if text in titles.keys():
-        passageId=titles[text]
+        passage_id=titles[text]
     else:
         say("没有您要背的诗哦，我们随机背一首吧")
 
-    passage=getCHNPassageById(passageId)
-    print(passage)
+    passage=getCHNPassageById(passage_id)
+    # print(passage)
+    say(passage['title'])
+    say(passage['writer'])
+    sentences=re.split('，|。|,|\.',passage['content'])
+    for s in sentences:
+        while True:
+            say(s)
+            ans=listen().replace('。','')
+            # ans=input()
+            if s in ans:
+                break
+            else:
+                say("不对哦，再来一遍")
+    say("跟背完毕")
+
 
 if __name__=="__main__":
     recite_by_sentence()
